b971b07b6d9652260744fac9e2290744823d60e8
Davidon4 <Quayson450@gmail.com>
2022-11-14 04:52:43 +0100

    'update-file'

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -4,7 +4,6 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../screens/Home";
 import CountryDetails from "../screens/CountryDetails";
 import List from "../screens/List";
-import FiltersScreen from "../screens/FiltersScreen";
 import HeaderTab from "./HeaderTab";
 import HeaderTitle from "./HeaderTitle";
 
@@ -41,16 +40,6 @@ export default function ScreenStack() {
           options={{
             headerTitleAlign: "center",
             title: "List",
-            headerShadowVisible: false,
-            headerBackTitle: "Back",
-          }}
-        />
-        <Stack.Screen
-          name="FiltersScreen"
-          component={FiltersScreen}
-          options={{
-            headerTitleAlign: "center",
-            title: "FiltersScreen",
             headerShadowVisible: false,
             headerBackTitle: "Back",
           }}
