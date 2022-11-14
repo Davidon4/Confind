a7cd4b7f29e73ec50d65b59d2154945c35f256c2
Davidon4 <Quayson450@gmail.com>
2022-11-14 07:58:08 +0100

    'final-update'

--- components/AppBar/index.js
+++ components/AppBar/index.js
@@ -4,6 +4,7 @@ import ThemeContext from "../../config/ThemeContext";
 import { AntDesign } from "@expo/vector-icons";
 import { styles } from "./styles";
 import { useNavigation } from "@react-navigation/native";
+import InitialFilter from "../../feature/initialFilter";
 
 const AppBar = ({ setFilteredValue }) => {
   const theme = useContext(ThemeContext);
@@ -13,10 +14,7 @@ const AppBar = ({ setFilteredValue }) => {
       <TouchableOpacity style={styles.iconContainer1}>
         <AntDesign name="earth" size={24} color={theme.list} />
       </TouchableOpacity>
-      <TouchableOpacity style={styles.iconContainer2}>
-        <AntDesign name="filter" size={24} color={theme.list} />
-        <Text style={{ color: theme.list }}>Filter</Text>
-      </TouchableOpacity>
+      <InitialFilter setFilteredValue={setFilteredValue} />
     </View>
   );
 };
--- feature/initialFilter.js
+++ feature/initialFilter.js
@@ -1,72 +1,71 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Dimensions } from "react-native";
+import React, { useState, useEffect, useContext } from "react";
+import { StyleSheet, Dimensions, Text, TouchableOpacity } from "react-native";
 import RNPickerSelect from "react-native-picker-select";
+import { AntDesign } from "@expo/vector-icons";
+import ThemeContext from "../config/ThemeContext";
 
 const initialFilter = ({ setFilteredValue }) => {
+  const theme = useContext(ThemeContext);
   const [currentValue, setCurrentValue] = useState(null);
   const [borderColor, setBorderColor] = useState("#ffffff22");
 
   useEffect(() => {
-    inputStyles.inputIOS = { ...inputStyles.inputIOS, borderColor };
+    styles.inputIOS = { ...styles.inputIOS, borderColor };
   }, [borderColor]);
 
   return (
-    <RNPickerSelect
-      style={inputStyles}
-      placeholder={{
-        label: "Filter by region",
-        value: null,
-      }}
-      items={[
-        {
-          label: "All",
-          value: "all",
-        },
-        {
-          label: "Europe",
-          value: "europe",
-        },
-        {
-          label: "Africa",
-          value: "africa",
-        },
-        {
-          label: "Asia",
-          value: "asia",
-        },
-        {
-          label: "Americas",
-          value: "americas",
-        },
-        {
-          label: "Oceania",
-          value: "oceania",
-        },
-      ]}
-      value={currentValue}
-      onValueChange={(value) => setCurrentValue(value)}
-      onDonePress={() => setFilteredValue(currentValue)}
-      onOpen={() => setBorderColor("#ffffff22")}
-      onClose={() => setBorderColor("#ffffff88")}
-    />
+    <TouchableOpacity style={styles.iconContainer2}>
+      <AntDesign name="filter" size={24} color={theme.list} />
+      <RNPickerSelect
+        placeholder={{
+          label: "Filter",
+          value: null,
+        }}
+        items={[
+          {
+            label: "All",
+            value: "all",
+          },
+          {
+            label: "Europe",
+            value: "europe",
+          },
+          {
+            label: "Africa",
+            value: "africa",
+          },
+          {
+            label: "Asia",
+            value: "asia",
+          },
+          {
+            label: "Americas",
+            value: "americas",
+          },
+          {
+            label: "Oceania",
+            value: "oceania",
+          },
+        ]}
+        value={currentValue}
+        onValueChange={(value) => setCurrentValue(value)}
+        onDonePress={() => setFilteredValue(currentValue)}
+        onOpen={() => setBorderColor("#ffffff22")}
+        onClose={() => setBorderColor("#ffffff88")}
+      />
+    </TouchableOpacity>
   );
 };
 
-const inputStyles = StyleSheet.create({
-  inputIOS: {
-    fontSize: 16,
-    paddingVertical: 12,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderRadius: 4,
-    borderColor: "#ffffff22",
-    color: "white",
-    width: Dimensions.get("window").width - 60,
-    marginLeft: 30,
-    marginTop: 10,
-  },
+const styles = StyleSheet.create({
   placeholder: {
     color: "#aaaaaa",
+  },
+  iconContainer2: {
+    height: 40,
+    flexDirection: "row",
+    padding: 8,
+    marginRight: 15,
   },
 });
 
